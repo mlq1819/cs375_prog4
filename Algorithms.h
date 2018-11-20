59b305f84018c7ecc9745e482ef925c0d2dd8b79
Michael Quinn <mquinn5@binghamton.edu>
2018-11-19 20:08:07 -0500

    disabling debug?

--- Algorithms.h
+++ Algorithms.h
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <string>
 #ifndef DEBUG
-#define DEBUG true
+#define DEBUG false
 #endif
 
 unsigned int max(unsigned int, unsigned int);
--- program4.cpp
+++ program4.cpp
@@ -11,7 +11,7 @@
 #include "Algorithms.h"
 #define PRESORT true
 #ifndef DEBUG
-#define DEBUG true
+#define DEBUG false
 #endif
 
 using namespace std;
